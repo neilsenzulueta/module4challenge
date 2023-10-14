9634c8d6233aedba52c2b10a1174df01662920a5
neilsen zulueta <neilzulueta@gmail.com>
2023-10-13 18:37:04 -0700

    added questions .js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,34 @@
+var questions = [
+    {
+        question: "Commonly used data types DO NOT Include:",
+        choices: ["1.strings", "2.Booleans", "3.Alerts", "4.numbers"],
+        correctAnswer: "option2",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ____.",
+        choices: ["1.quotes", "2.curly brackets", "3.parentheses", "4.square brackets"],
+        correctAnswer: "option2",
+    },
+    {
+        question: "Arrays in Javascript can be used to store _____.",
+        choices: ["1.numbers and strings", "2.other arrays", "3.booleans", "4.all of the above"],
+        correctAnswer: "option3",
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["1.commas", "2.curly brackets", "3.quotes", "4.parentheses"],
+        correctAnswer: "option0",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["1.javascript", "2.terminal/bash", "3.for loops", "4.console log"],
+        correctAnswer: "option3"
+    }
+]
+
 var startBtn = document.querySelector("start")
 var highscoreBtn =document.querySelector("highscores")
+
 
 startBtn.addEventListener("Click", startQuiz)
 
